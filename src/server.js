043d27b280474c776f85442d2614a04986aeacd1
vidhias17 <vidhias17@gmail.com>
2024-12-09 19:18:54 -0500

    Added Create endpoint for Car resource

--- src/server.js
+++ src/server.js
@@ -4,13 +4,40 @@ const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
+const filePath = './data/cars.json';
 
-// Middleware
 app.use(bodyParser.json());
 
-// Basic Route
-app.get('/', (req, res) => {
-    res.send('Welcome to the Simple API!');
+const readData = () => {
+    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+};
+
+const writeData = (data) => {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+};
+
+
+//Create 
+app.post('/cars', (req, res) => {
+    const cars = readData();
+    
+    const newCar = {
+        id: Date.now(), // Unique ID based on timestamp
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+    };
+
+    cars.push(newCar);
+    writeData(cars);
+
+    res.status(201).json(newCar);
+});
+
+//Get all cars
+app.get('/cars', (req, res) => {
+    const cars = readData();
+    res.json(cars);
 });
 
 // Start the server
